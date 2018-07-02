10fa897ea272c27e88ed43497c606e1274321b1f
Atefeh <atefeh.khoshnood@gmail.com>
2018-07-02 00:02:06 -0700

    clusterone features added

--- Influenza-model.py
+++ Influenza-model.py
@@ -6,11 +6,14 @@ from __future__ import unicode_literals
 import h5py
 import pandas
 import sklearn
-import numpy
-import six
-import joblib
 import rdkit 
-import mdtraj
+import time
+import os
+import logging
+import traceback
+import json
+import glob
+
 import tensorflow as tf
 import deepchem as dc
 import numpy as np
@@ -18,12 +21,45 @@ import pandas as pd
 
 from deepchem.models.tensorgraph.models.graph_models import GraphConvTensorGraph
 
-#data_path = get_data_path(
-#           dataset_name = "/Users/MAJ/Desktop/DeepChem/Influenza-A-virus-Active-NotActive.csv",
-#           local_root = te_path,
-#           local_repo = te_filename,
-#           path = ''
-#           )
+# What is your ClusterOne username? This should be something like "johndoe", not your email address!
+CLUSTERONE_USERNAME = "atefeh"
+
+# Where should your local log files be stored? This should be something like "~/Documents/self-driving-demo/logs/"
+LOCAL_LOG_LOCATION = "/Users/MAJ/Desktop/DeepChem"
+
+# Where is the dataset located? This should be something like "~/Documents/data/" if the dataset is in "~/Documents/data/comma"
+LOCAL_DATASET_LOCATION = "/Users/MAJ/Desktop/"
+
+# Name of the data folder. In the example above, "comma"
+LOCAL_DATASET_NAME = "DeepChem"
+
+#clusterone
+from clusterone import get_data_path, get_logs_path
+
+try:
+	job_name = os.environ['JOB_NAME']
+	task_index = os.environ['TASK_INDEX']
+	ps_hosts = os.environ['PS_HOSTS']
+	worker_hosts = os.environ['WORKER_HOSTS']
+except:
+	job_name = None
+	task_index = 0
+	ps_hosts = None
+	worker_hosts = None
+
+if job_name == None: #if running locally
+	if LOCAL_LOG_LOCATION == "...":
+    	raise ValueError("LOCAL_LOG_LOCATION needs to be defined")
+    if LOCAL_DATASET_LOCATION == "...":
+        raise ValueError("LOCAL_DATASET_LOCATION needs to be defined")
+    if LOCAL_DATASET_NAME == "...":
+        raise ValueError("LOCAL_DATASET_NAME needs to be defined")
+
+#Path to your data locally. This will enable to run the model both locally and on
+# ClusterOne without changes
+PATH_TO_LOCAL_LOGS = os.path.expanduser(LOCAL_LOG_LOCATION)
+ROOT_PATH_TO_LOCAL_DATA = os.path.expanduser(LOCAL_DATASET_LOCATION)
+#end of clusterone snippet 1
 
 graph_featurizer = dc.feat.graph_features.ConvMolFeaturizer()
 loader = dc.data.data_loader.CSVLoader( tasks=['ACTIVITY_CLASS'], smiles_field="CANONICAL_SMILES", id_field="CMPD_CHEMBLID", featurizer=graph_featurizer)
