8f1407eed537f96573de6529761798838441db43
Atefeh <atefeh.khoshnood@gmail.com>
2018-07-04 13:20:37 -0700

    test

--- test.py
+++ test.py
@@ -20,19 +20,23 @@ def install_pip_import(package):
     finally:
         globals()[package] = importlib.import_module(package)
 
-def install_conda_import(package):
+def install_conda_import(channel,package):
     import importlib
     try:
         importlib.import_module(package)
     except ImportError:
         import conda.cli
-        conda.cli.main('conda','install','-y', package)
+        conda.cli.main('conda','install','-y','c', channel, package)
     finally:
         globals()[package] = importlib.import_module(package)
 
 def main():
-    install_conda_import('rdkit')
-    install_pip_import('clusterone')
+    install_conda_import('conda-forge','flaky=3.3.0')
+    install_conda_import('conda-forge','joblib=0.11')
+    install_conda_import('conda-forge','jupyter=1.0.0.*')
+    install_conda_import('conda-forge','mdtraj=1.9.1')
+    install_conda_import('rdkit','rdkit=2017.09.1')
+    install_pip_import('clusterone==0.11.2')
 
 if __name__ == "__main__":
     main()
