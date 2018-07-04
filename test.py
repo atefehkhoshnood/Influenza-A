a8d8bb636b22e84208659b24d7d71daff77c87ad
Atefeh <atefeh.khoshnood@gmail.com>
2018-07-04 12:44:27 -0700

    test

--- test.py
+++ test.py
@@ -4,14 +4,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-import pip
 import time
 import os
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-def install_and_import(package):
+def install_pip_import(package):
     import importlib
     try:
         importlib.import_module(package)
@@ -21,9 +20,19 @@ def install_and_import(package):
     finally:
         globals()[package] = importlib.import_module(package)
 
+def install_conda_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import conda.cli
+        conda.cli.main('conda','install','-y', package)
+    finally:
+        globals()[package] = importlib.import_module(package)
+
 def main():
-    install_and_import('rdkit')
-    install_and_import('clusterone')
+    install_conda_import('rdkit')
+    install_pip_import('clusterone')
 
 if __name__ == "__main__":
     main()
