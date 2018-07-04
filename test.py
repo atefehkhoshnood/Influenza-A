e28469a00f15a7e78d2c434e49b2c180f4ac35c5
Atefeh <atefeh.khoshnood@gmail.com>
2018-07-04 11:43:46 -0700

    test

--- test.py
+++ test.py
@@ -3,27 +3,27 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-conda install -y -q -c omnia pdbfixer=1.4
-conda install -y -q -c deepchem mdtraj=1.9.1
-conda install -y -q -c rdkit rdkit=2017.09.1
-conda install -y -q -c conda-forge joblib=0.11 \
-    six=1.11.0 \
-    scikit-learn=0.19.1 \
-    networkx=2.1 \
-    pillow=5.0.0 \
-    pandas=0.22.0 \
-    nose=1.3.7 \
-    nose-timer=0.7.0 \
-    flaky=3.3.0 \
-    zlib=1.2.11 \
-    requests=2.18.4 \
-    xgboost=0.6a2 \
-    simdna=0.4.2 \
-    jupyter=1.0.0 \
-    pbr=3.1.1 \
-    setuptools=39.0.1 \
-    biopython=1.71 \
-    numpy=1.14
+#conda install -y -q -c omnia pdbfixer=1.4
+#conda install -y -q -c deepchem mdtraj=1.9.1
+#conda install -y -q -c rdkit rdkit=2017.09.1
+#conda install -y -q -c conda-forge joblib=0.11 \
+#    six=1.11.0 \
+#    scikit-learn=0.19.1 \
+#    networkx=2.1 \
+#    pillow=5.0.0 \
+#    pandas=0.22.0 \
+#    nose=1.3.7 \
+#    nose-timer=0.7.0 \
+#    flaky=3.3.0 \
+#    zlib=1.2.11 \
+#    requests=2.18.4 \
+#    xgboost=0.6a2 \
+#    simdna=0.4.2 \
+#    jupyter=1.0.0 \
+#    pbr=3.1.1 \
+#    setuptools=39.0.1 \
+#    biopython=1.71 \
+#    numpy=1.14
 
 pip install clusterone==0.11.2
 
